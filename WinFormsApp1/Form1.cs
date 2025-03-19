d35c2bcd407875b971812f91f2a2e9ae73e1eb0c
Samantha Skiba <skibas@mymacewan.ca>
2025-03-18 17:35:00 -0700

    Login UI

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,25 @@
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class EnterUP : Form
     {
-        public Form1()
+        public EnterUP()
         {
             InitializeComponent();
+        }
+
+        private void UserTitle_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employee_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
