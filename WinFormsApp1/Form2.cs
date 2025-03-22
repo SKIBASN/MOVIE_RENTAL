a5853843953d67d3f6b75cea25363f7718cf52a1
Samantha Skiba <skibas@mymacewan.ca>
2025-03-21 23:01:48 -0700

    report UI

--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -31,5 +31,53 @@ namespace WinFormsApp1
         {
 
         }
-    }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ReportSelection.SelectedItem.ToString() == "What Movies haven't been rented since a specific date?")
+            {
+                cal.Visible = true;
+                DateSelect.Visible = true;
+                Specif.Visible = false;
+                SpecifTitle.Visible = true;
+                SpecifTitle.Text = "Pick a Date";
+            }
+            else if (ReportSelection.SelectedItem.ToString() == "What movies has a specific employee rented?")
+            {
+                Specif.Visible = true;
+                DateSelect.Visible = false;
+                cal.Visible = false;
+                SpecifTitle.Visible = true;
+                SpecifTitle.Text = "Enter an Employee ID";
+            }
+            else if (ReportSelection.SelectedItem.ToString() == "What Actors appear in a Movie?")
+            {
+                cal.Visible = false;
+                DateSelect.Visible = false;
+                Specif.Visible = true;
+                SpecifTitle.Visible = true;
+                SpecifTitle.Text = "Enter a Movie ID";
+            }
+            else if (ReportSelection.SelectedItem.ToString() == "How many times has a movie with a specific actor been rented?")
+            {
+                cal.Visible = false;
+                Specif.Visible = true;
+                DateSelect.Visible = false;
+                SpecifTitle.Visible = true;
+                SpecifTitle.Text = "Enter an Actor ID";
+            }
+            else
+            {
+                cal.Visible = false;
+                Specif.Visible = false;
+                DateSelect.Visible = false;
+                SpecifTitle.Visible = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }      
 }
